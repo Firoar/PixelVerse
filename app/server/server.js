5701152f9f8401207bdf6902168411da22f8c892
Dr.C_XOR <149101313+Firoar@users.noreply.github.com>
2025-02-09 21:28:05 +0530

    Update server.js

--- app/server/server.js
+++ app/server/server.js
@@ -43,7 +43,7 @@ const sessionMiddleware = session({
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: false,
     sameSite: "strict",
     maxAge: 1000 * 60 * 60 * 24,
   },
