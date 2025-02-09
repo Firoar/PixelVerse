fc535e7065a5987905b5f73ea8a1421a9389291b
Firoar <chiru02.dev@gmail.com>
2025-02-09 22:32:53 +0530

    mini changes

--- app/client/src/components/protected/ProtectedRoute.jsx
+++ app/client/src/components/protected/ProtectedRoute.jsx
@@ -2,18 +2,16 @@ import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router";
 import axios from "axios";
 
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 const ProtectedRoute = ({ children }) => {
   const [isAuth, setIsAuth] = useState(null);
 
   useEffect(() => {
     const checkAuth = async () => {
       try {
-        const response = await axios.get(
-          "http://localhost:3000/api/Protected/",
-          {
-            withCredentials: true,
-          }
-        );
+        const response = await axios.get(`${SERVER_URL}/api/Protected/`, {
+          withCredentials: true,
+        });
 
         if (response.data.ok) {
           setIsAuth(true);
